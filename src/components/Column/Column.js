1aa70e744d24bae65cc631dfcfb81f602f09d92c
Rıdvan Karadağ <ridvan.karadag@std.izu.edu.tr>
2019-02-17 02:01:48 +0300

    Update Column.js

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,12 +1,10 @@
 import styled from "styled-components";
 
 const Column = styled.div`
-width: ${props => `calc((100% / 12) * ${props.gap})` || "100%"};
-min-height: 50px;
-background-color: yellow;
-:last-child {
-background-color: purple;
-}
+    width: ${props => `calc((100% / 12) * ${props.gap} - 10px)` || "100%"};
+    min-height: 50px;
+    margin: 0 5px;
+    margin-right: ${props => `calc((100% / 12) * ${props.rightSpace} - 10px)` || "5px"}
 `;
 
 export default Column;
