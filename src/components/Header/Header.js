faa1f25ed499b32d5fad778cd81f4c50a33db4c0
Elif Çilingir <cilingir43@gmail.com>
2019-06-18 17:10:13 +0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,6 @@ const Header = styled.header`
     background-position: 0%;
     background-size: cover;
     color: ${props => props.textColor || "#fff"};
-    box-shadow: inset 0 -10px 8px #00000030;
     :hover {
       opacity: .98;
     }
