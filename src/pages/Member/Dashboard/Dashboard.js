20dd8691598acc4959301a793c5b7bb3cfb55558
Elif Çilingir <cilingir43@gmail.com>
2019-08-05 16:01:26 +0300

    Update Dashboard.js

--- src/pages/Member/Dashboard/Dashboard.js
+++ src/pages/Member/Dashboard/Dashboard.js
@@ -1,22 +1,48 @@
 import React, { Fragment } from "react";
+import {Link} from "react-router-dom";
 import Layout from "../../../components/Layout/Layout";
-import HeaderText from "../../../components/HeaderText/HeaderText";
 import Header from "../../../components/Header/Header";
 import FlexWrapper from "../../../components/FlexWrapper/FlexWrapper";
 import BoxWrapper from "../../../components/BoxWrapper/BoxWrapper";
 import Box from "../../../components/Box/Box";
 import BoxTitle from "../../../components/BoxTitle/BoxTitle";
-
+import Avatar from "../../../assets/images/avatar.jpg";
 function Dashboard() {
-
-
     return (
         <Fragment>
-            <Header headerColor="#2c3e50">
+            <Header headerColor="#d1d8e0">
                 <Layout>
-                    <HeaderText>
-                        Dashboard
-                    </HeaderText>
+                    <div style={{height:`6rem`,position: 'relative', padding: `0 1.5rem`,}}>
+                        <div className="header_content">
+                            <div className="header_left"><h4 style={{ color: '#f50057',marginTop:'inherit', fontSize: '3rem'}}>Dashboard</h4></div>
+                            <div className="header_right">
+                                <ul>
+                                    <li className="icon">
+                                        <a className="user_info">
+                                            <img src={Avatar}></img>
+                                            <span>Elif Çilingir</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="navbar" >
+                        <ul className="menu">
+                            <li className="menu_item">
+                             <Link to="/dashboard/departments">Departments</Link>
+                            </li>
+                            <li className="menu_item">
+                                <Link to="/dashboard/jobs">Jobs</Link>
+                            </li>
+                            <li className="menu_item">
+                                <Link to="/dashboard/jobs/create">Post a Job</Link>
+                            </li>
+                            <li className="menu_item">
+                                <Link  to="/dashboard/users">Users</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </Layout>
             </Header>
             <Layout>
