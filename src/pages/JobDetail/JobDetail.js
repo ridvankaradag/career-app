afca74083cbea2df095b6c29c820cb489d506352
Elif Çilingir <cilingir43@gmail.com>
2019-08-05 16:01:58 +0300

    Update JobDetail.js

--- src/pages/JobDetail/JobDetail.js
+++ src/pages/JobDetail/JobDetail.js
@@ -10,6 +10,7 @@ import PageFooter from "../../components/PageFooter/PageFooter";
 import Button from "../../components/Button/Button";
 import { connect } from "react-redux";
 import { getJob } from "../../js/actions/index";
+import Column from "../../components/Column/Column";
 
 
 
@@ -20,80 +21,103 @@ function mapDispatchToProps(dispatch) {
 }
 class JobDetail extends Component {
     componentDidMount(){
-        const { getJob  } = this.props;
+        const { getJob } = this.props;
         getJob(this.props.match.params.id);
-        console.log(this.props)
-       
-  
       }
     render(props) {
-
+        const jobData  = this.props.location.jobParams.job;
+        console.log(jobData)
         return (
             <Fragment>
                 <Header>
-                    <Layout>
-                        <HeaderTextLink to="/">
-                            Company Title
-                        </HeaderTextLink>
+                    <Layout> 
+                        
+                            <HeaderTextLink to="/">
+                                { jobData.company_name }
+                            </HeaderTextLink>
+                    
+                    
                     </Layout>
                 </Header>
                 <Layout>
                     <PageHeader>
-                        <PageTitle>Back-End Engineer</PageTitle>
-                        <PageSubTitle>Product - Remote</PageSubTitle>
+                        <div style={{    
+                            backgroundPosition: '50%',
+                            backgroundSize: 'contain',
+                            width: '8.125rem',
+                            height: '8.125rem',
+                            overflow: 'hidden',
+                            borderRadius: '0.625rem',
+                            margin: 'auto',
+                            marginBottom: '1.875rem',
+                            marginTop: '3.75rem',
+                            backgroundRepeat: 'no-repeat',
+                            backgroundColor: '#fff',
+                            }}></div>
+                        <PageTitle>{ jobData.position }</PageTitle>
+                        <h6 style={{
+                            opacity: '.5',
+                            fontSize: '1.5rem',
+                            letterSpacing: '.1125rem',
+                            marginBottom: '3.75rem',
+                            textAlign: 'center',
+                            textTransform: 'uppercase',
+                            fontWeight: '700',
+                            }}>
+                                { jobData.company_name }
+                            </h6>
+                        <PageSubTitle>{ jobData.location }</PageSubTitle>
                     </PageHeader>
                     <PageContent>
-                        <div className="job-emcasa">
-                            <p>A EmCasa nasceu para <strong>transformar a experiência de comprar
-                            e vender um imóvel no Brasil. </strong>Nós usamos tecnologia, inteligência de dados e um
-                            modelo de negócio inovador para redefinir o mercado brasileiro de Real Estate em favor dos
-                            compradores e vendedores.</p><p>Consumidores precisam lidar com serviços de baixa qualidade,
-                            falta de transparência, documentação complexa, burocracia, problemas de segurança e diversos
-                            outros. Além disso, devem pagar altas taxas de 5% a 6% sobre o valor do imóvel.</p><p>Todos
-                            os dias, tempo e dinheiro são desperdiçados por causa das ineficiências atuais, o que é
-                            péssimo para o país - A EmCasa surgiu para resolver este problema e transformar o setor
-                            através da tecnologia, produto e um modelo de negócios inovador.</p>
-                            <h3>Responsabilidades</h3>
-                            <p>Como Back-End engineer você será responsável pelo desenvolvimento das nossas APIs e
-                                serviços internos
-                                que atendem as aplicações dos nossos clientes e times internos.</p>
-                            <p>Nosso stack é composto por Elixir, Phoenix, Absinthe (GraphQL), PostgreSQL &amp; ELK.
-                                Além das aplicações, você também será responsável por manter em conjunto com o time
-                                nossa infra-estrutura,
-                                desde a arquitetura até a manutenção.</p>
-                            <h3>Requisitos</h3>
-                            <ul>
-                                <li>Conhecimento em Elixir e/ou linguagens funcionais;</li>
-                                <li>Conhecer PostgreSQL (ou similar);</li>
-                                <li>Experiência com webapps &amp; seu foundation: HTTP, TCP/IP, segurança, browser e
-                                    outros clientes;
-                                </li>
-                                <li>Conhecer testes unitários &amp; end-to-end da aplicação;</li>
-                                <li>Ter conhecimento de boas práticas de arquitetura de aplicação e organização de
-                                    código;
-                                </li>
-                                <li>Ter comprometimento e ownership em organizar o seu próprio tempo e entregas, não
-                                    fazemos micromanagement;
-                                </li>
-                                <li>Acreditar em decisões orientadas a dados - e ser responsável por ajudar a coletar,
-                                    planejar e mantê-los;
-                                </li>
-                            </ul>
-                            <h3>Diferenciais</h3>
-                            <ul>
-                                <li>Experiência com ELK (ElasticSearch + Logstash + Kibana);</li>
-                                <li>Experiência com ferramentas de monitoramento;</li>
-                                <li>Experiência com data-pipelines e/ou aplicações com machine learning;</li>
-                                <li>Experiência com infra-estrutura na AWS (ou similar);</li>
-                                <li>Experiência em startups e times de produto;</li>
-                            </ul>
-                            <p>Somos open-source! Cheque nosso repositório</p>
-                        </div>
+                        <div className="row" style={{flexDirection: 'row'}}>
+                        <div className="col-md-12 col-lg-4">
+                                <div style={{
+                                        lineHeight: '1.6em'
+                                    }}>
+                                        <div style={{
+                                            backgroundColor: '#fff',
+                                            boxShadow: '0 0.125rem 0.9375rem 0 rgba(0,0,0,.1)',
+                                            padding: '1.25rem',
+                                            marginBottom: '1.125rem',
+                                        }}>
+                                            <div style={{display:'flex',flexDirection:'column', alignItems: 'center'}}>
+                                                <h6>Company</h6>
+                                                <div style={{marginBottom: 15}}>{jobData.company_name}</div>
+                                                <h6>Location</h6>
+                                                <div  style={{marginBottom: 15}}>{jobData.location}</div>
+                                                <h6>Type of Position</h6>
+                                                <div>{jobData.job_type ==='full_time' && 'Full Time'}</div>
+
+                                            </div>
+                                        </div>
+                                        <Button href={"mailto:" + jobData.company_email} >Apply</Button>
+                                </div>
+                            </div>
+                            <div className="col-md-12 col-lg-8">
+                                <div style={{
+                                    lineHeight: '1.6em'
+                                }}>
+                                    <div style={{
+                                        backgroundColor: '#fff',
+                                        boxShadow: '0 0.125rem 0.9375rem 0 rgba(0,0,0,.1)',
+                                        
+                                        padding: '3.125rem',
+                                        marginBottom: '1.875rem',
+                              
+                                    }}>
+                                     <div className="job-emcasa" dangerouslySetInnerHTML={{__html: jobData.description}}></div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            
+                    </div>
                     </PageContent>
                     <PageFooter>
-                        <Button href="mailto:a@b.com">Apply</Button>
+                        
                     </PageFooter>
                 </Layout>
+            
             </Fragment>
         )
     }
