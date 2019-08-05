851cf6a408e4b1586990111f6e773c6992b94ce9
Elif Çilingir <cilingir43@gmail.com>
2019-08-05 16:01:14 +0300

    Update Departments.js

--- src/pages/Member/Departments/Departments.js
+++ src/pages/Member/Departments/Departments.js
@@ -1,18 +1,15 @@
 import React, {Fragment, useState} from "react";
+import {Link} from "react-router-dom";
 import Header from "../../../components/Header/Header";
 import Layout from "../../../components/Layout/Layout";
-import HeaderTextLink from "../../../components/HeaderTextLink/HeaderTextLink";
-import Column from "../../../components/Column/Column";
-import FlexWrapper from "../../../components/FlexWrapper/FlexWrapper";
-import FormInputDefault from "../../../components/FormInputDefault/FormInputDefault";
-import FormInputButton from "../../../components/FormInputButton/FormInputButton";
-import FormDefault from "../../../components/FormDefault/FormDefault";
-import TableDefault from "../../../components/TableDefault/TableDefault";
 import { connect } from "react-redux"
 import { deleteDepartment } from "../../../js/actions/index";
 import { updateDepartment } from "../../../js/actions/index";
 import { addDepartment } from "../../../js/actions/index";
-
+import Avatar from "../../../assets/images/avatar.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenAlt } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 const mapStateToProps = state => {
     return { 
@@ -80,16 +77,116 @@ function Departments(props) {
 
     return (
         <Fragment>
-            <Header headerColor="#2c3e50">
+            <Header headerColor="#d1d8e0">
                 <Layout>
-                    <HeaderTextLink to="/dashboard">
-                        Departments
-                    </HeaderTextLink>
+                    <div style={{height:`6rem`,position: 'relative', padding: `0 1.5rem`,}}>
+                        <div className="header_content">
+                            <div className="header_left"><Link to="/dashboard"><h4 style={{ color: '#f50057',marginTop:'inherit', fontSize: '3rem'}}>Departments</h4></Link></div>
+                            <div className="header_right">
+                                <ul>
+                                    <li className="icon">
+                                        <a className="user_info">
+                                            <img src={Avatar}></img>
+                                            <span>Elif Çilingir</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="navbar" >
+                        <ul className="menu">
+                            <li className="menu_item">
+                             <Link to="/dashboard/departments">Departments</Link>
+                            </li>
+                            <li className="menu_item">
+                                <Link to="/dashboard/jobs">Jobs</Link>
+                            </li>
+                            <li className="menu_item">
+                                <Link to="/dashboard/jobs/create">Post a Job</Link>
+                            </li>
+                            <li className="menu_item">
+                                <Link  to="/dashboard/users">Users</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </Layout>
             </Header>
-            <Layout>
+            <div style={{    marginTop: `-1.5rem`,marginLeft: `0` }}>
+                <div style={{
+                    width: `100%`,
+                    padding: `1.5rem 3rem 0`,
+                    marginRight: `auto`,
+                    marginLeft: `auto`,
+                }}>
+                    <div className="row">
+                        <div className="col-lg-12 col-xxl-12 col-xl-5">
+                            <div className="row">
+                                <div className="col-xl-12 col-sm-6 col-xxl-6">
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <h4 style={{marginBottom:20}}>New Department</h4>
+                                            <form onSubmit={handleSubmit}>
+                                                <div className="row">
+                                                    <div className="col-lg-12">
+                                                        <div className="form-group">
+                                                            <input type="text"  className="form-control bg-white text-muted"  placeholder="Department Name" value={value} onChange={e => setValue(e.target.value)}  required/>
+                                                        </div>
+                                                        <div class="text-right">
+                                                            <button class="btn btn-primary btn-sl-lg"  type="submit" value={update === null ? 'Save' : 'Update'} >Save</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-7 col-lg-12 col-xxl-12">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="card transparent-card">
+                                        <div className="card-body p-0">
+                                        {
+                                            departments.length > 0 &&
+                                            <table>
+                                                <thead>
+                                                    <tr>
+                                                        <th>Department</th>
+                                                        <th>Action</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                {
+                                                    departments.map((department) => (
+                                                        <tr key={department.id}>
+                                                            <td>{department.name}</td>
+                                                            <td>
+                                                                <span className="action_icon" onClick={() => editDepartment(department)}><FontAwesomeIcon icon={faPenAlt} /> </span> 
+                                                                <span className="action_icon ml-2" onClick={() => removeDepartment(department.id)}> <FontAwesomeIcon icon={faTrash} /></span>
+                                                            </td>
+                                                        </tr>
+                                                    ))
+                                                }
+                                                </tbody>
+                                            </table>
+                                        }
+                                        {
+                                         departments.length <= 0 &&
+                                             <p>You have not added any department yet.</p>
+                                        }
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            { /* <Layout>
                 <FlexWrapper>
-                    <Column gap="5" rightSpace="1">
+                  <Column gap="5" rightSpace="1">
                         <FormDefault onSubmit={handleSubmit}>
                             <FlexWrapper>
                                 <FormInputDefault type="text" placeholder="Department Name" value={value} onChange={e => setValue(e.target.value)} required/>
@@ -132,7 +229,7 @@ function Departments(props) {
                         </FlexWrapper>
                     </Column>
                 </FlexWrapper>
-            </Layout>
+            </Layout>*/}
         </Fragment>
     );
 
